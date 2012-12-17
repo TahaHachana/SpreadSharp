cbadddca30278345d813e339670f2a0c992da337
Taha Hachana <tahahachana@live.com>
2012-12-17 13:28:09 +0100

    delete fsx file

--- SpreadSharp/SpreadSharp/Script.fsx
+++
@@ -1,7 +0,0 @@
-﻿// Ce fichier est un script qui peut être exécuté avec F# Interactive.  
-// Il permet d'explorer et de tester le projet de bibliothèque.
-// Notez que les fichiers de script ne feront pas partie de la build de projet.
-
-#load "Module1.fs"
-open Module1
-
