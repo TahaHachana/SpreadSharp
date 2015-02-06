7d52d5b65c7056f19c174beae9c58fc8f018f167
Taha Hachana <tahahachana@gmail.com>
2015-02-06 15:06:51 +0100

    Upgrade to VS 2013

--- SpreadSharp/SpreadSharp/Records.fs
+++ SpreadSharp/SpreadSharp/Records.fs
@@ -2,46 +2,46 @@
 
 module Records =
 
-    let private recordsRange headers fields worksheet =
-        let columnsCount = Array.length headers
-        let length = Seq.length fields + 1 |> string
-        let rangeString = String.concat "" ["A1:"; string (char (columnsCount + 64)) + length]
-        XlRange.get worksheet rangeString
+    open Microsoft.FSharp.Reflection
+    open Collections
 
-    let private displayRecords records recordType range =
-        let headers = Utilities.recordFieldsNames recordType
-        let fields = Utilities.fieldsArray records
-        let array = Array2D.ofSeqs headers fields
-        XlRange.setValue range array
-
-    let private displayRecords' records recordType worksheet =
-        let headers = Utilities.recordFieldsNames recordType
-        let fields = Utilities.fieldsArray records
-        let array = Array2D.ofSeqs headers fields
-        let range = recordsRange headers fields worksheet
-        XlRange.setValue range array
-
-    /// <summary>Sends the values of a collection of F# records to an Excel range.</summary>
-    /// <param name="rcords">The F# records array.</param>
-    /// <param name="recordType">The type of the record.</param>         
-    /// <param name="range">The range object.</param>         
-    let toRange (records : 'T seq) recordType range = displayRecords records recordType range
+    let private fields record =
+        FSharpValue.GetRecordFields record
+        |> Array.map (fun x ->
+            match x with
+            | :? string as str -> str
+            | _ -> x.ToString()
+        )
 
     /// <summary>Sends the values of a collection of F# records to an Excel worksheet.</summary>
     /// <param name="rcords">The F# records collection.</param>
     /// <param name="recordType">The type of the record.</param>         
     /// <param name="worksheet">The worksheet object.</param>
-    let toWorksheet (records : 'T seq) recordType worksheet = displayRecords' records recordType worksheet
+    let toWorksheet worksheet (records: seq<'T>) =
+        let headers =
+            FSharpType.GetRecordFields typeof<'T>
+            |> Array.map (fun x -> x.Name)
+        let columnsCount = Array.length headers
+        let rangeString = String.concat "" ["A1:"; string (char (columnsCount + 64)) + "1"]
+        let rng = XlRange.get worksheet rangeString
+        Array.toRange rng headers
+        records
+        |> Seq.iteri (fun idx x ->
+            let idxString = string <| idx + 2
+            let rangeString = String.concat "" ["A"; idxString; ":"; string (char (columnsCount + 64)); idxString]
+            let rng = XlRange.get worksheet rangeString
+            Array.toRange rng <| fields x
+        )
 
     /// <summary>Saves a collection of records in a workbook using the specified file name.</summary>
     /// <param name="rcords">The records collection.</param>
     /// <param name="recordType">The type of the record.</param>         
     /// <param name="filename">The destination file name.</param>
-    let saveAs records recordType filename =
+    let saveAs filename records =
         let app = XlApp.start ()
         let wb = XlWorkbook.add app
         let ws = XlWorksheet.byIndex wb 1
-        toWorksheet records recordType ws
+        toWorksheet ws records
         XlWorkbook.saveAs wb filename
         XlWorkbook.close wb
         XlApp.quit app
